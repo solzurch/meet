8406f6214ad0756a9338a5b5c2c9ab2bdf20e73e
solzurch <151952240+solzurch@users.noreply.github.com>
2024-04-18 13:38:10 +0200

    4.5

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -5,7 +5,9 @@ import NumberOfEvents from "../components/NumberOfEvents";
 describe("<NumberOfEvents /> component", () => {
   let NumberOfEventsComponent;
   beforeEach(() => {
-    NumberOfEventsComponent = render(<NumberOfEvents />);
+    NumberOfEventsComponent = render(
+      <NumberOfEvents setCurrentNOE={() => {}} />
+    );
   });
 
   test("renders number of events text input", () => {
@@ -20,11 +22,8 @@ describe("<NumberOfEvents /> component", () => {
   });
 
   test("number of events text box value changes when the user types in it", async () => {
-    const user = userEvent.setup();
-    const numberTextBox = NumberOfEventsComponent.queryByRole("textbox");
-    await user.type(numberTextBox, "123");
-
-    // 32 (the default value already written) + 123
-    expect(numberTextBox).toHaveValue("32123");
+    const numberTextbox = NumberOfEventsComponent.queryByRole("textbox");
+    await userEvent.type(numberTextbox, "{backspace}{backspace}10");
+    expect(numberTextbox.value).toBe("10");
   });
 });
--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
-const NumberOfEvents = ({}) => {
-  const [number, setNumber] = useState(32);
+const NumberOfEvents = ({ setCurrentNOE }) => {
+  const [number, setNumber] = useState("32");
 
   const handleInputChanged = (event) => {
     const value = event.target.value;
     setNumber(value);
+    setCurrentNOE(value);
   };
 
   return (
